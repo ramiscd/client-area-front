b1717b120753c476b601d8d7d53fd6652079da47
Ramis Damasceno <ramis1998d@gmail.com>
2025-02-05 13:14:34 -0300

    fix layout and add paper

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { AppBar, Box, Button, IconButton, Step, StepLabel, Stepper, Toolbar, Typography } from "@mui/material";
-import { AccessAlarm, ThreeDRotation, Menu } from "@mui/icons-material";
+import { AppBar, Box, Button, IconButton, Paper, Step, StepLabel, Stepper, Toolbar, Typography } from "@mui/material";
+import { AccessAlarm, ThreeDRotation, Menu, CalendarMonth } from "@mui/icons-material";
 
 const steps = [
   'Ficha de visto pendente',
@@ -11,11 +11,11 @@ const steps = [
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-slate-100 h-screen">
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2}}>
-            <Menu/>      
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Menu />
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
             Client area
@@ -23,29 +23,41 @@ export default function Home() {
         </Toolbar>
       </AppBar>
 
-      <Typography>Status da applicação: Analizando documentos</Typography>
-      <div>
-        <Typography>
-          Sua entrevista está prevista para o dia 5 de março
-        </Typography>
+      <div className="m-6">
+
+        <Typography className="text-black">Status da applicação: Analizando documentos</Typography>
+        <div className="flex">
+          <CalendarMonth sx={{ color: "black" }} />
+          <Typography className="text-black">
+            Sua entrevista está prevista para o dia 5 de março
+          </Typography>
+        </div>
+        <div className="flex">
+          <CalendarMonth sx={{ color: "black" }} />
+          <Typography className="text-black">
+            O recebimento do seu passaporte está previsto para o dia 5 de março
+          </Typography>
+        </div>
+
+
+
+        <Box sx={{ width: '100%' }} className="my-10">
+          <Stepper activeStep={1} alternativeLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+
+        <Paper elevation={3}>
+          <h1>Oi</h1>
+        </Paper>
+
       </div>
-      <div>
-        <Typography>
-          O recebimento do seu passaporte está previsto para o dia 5 de março
-        </Typography>
-      </div> 
 
 
-
-      <Box sx={{ width: '100%'}} className="my-10">
-        <Stepper activeStep={1} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
     </div>
   );
 }
