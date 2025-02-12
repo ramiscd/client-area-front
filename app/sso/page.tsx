16420bb8ab6c4c78f454a9398e193c416862a981
Ramis Damasceno <ramis1998d@gmail.com>
2025-02-12 07:07:18 -0300

    loging page start

--- app/sso/page.tsx
+++ app/sso/page.tsx
@@ -1,9 +1,16 @@
+import { Button, Input, Paper } from "@mui/material";
 
 //app/sso/sign-in.tsx
 export default function SignIn() {
   return (
-    <div>
-      <h1>SSO</h1>
+    <div className="flex justify-center items-center h-screen flex-col bg-gray-400">
+      <Paper elevation={20} className="w-fit flex flex-col items-center p-10 gap-6">
+        <h1>Login</h1>
+        <p>Bem vindo, por favor insira suas credenciais</p>
+        <Input placeholder="Email"/>
+        <Input placeholder="Senha"/>
+        <Button variant="contained">Acessar</Button>
+      </Paper>
     </div>
   )
 }
